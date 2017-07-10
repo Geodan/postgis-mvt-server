12aae9617c14c16a03fb671243a49b7e689cf625
Rubio Vaughan <rubio.vaughan@geodan.nl>
2017-07-10 17:51:26 +0200

    intermediate commit for upload support

--- mvt_server.js
+++ mvt_server.js
@@ -1,23 +1,26 @@
-var compression = require('compression');
-var express = require('express');
-var app = express();
-var Pool = require('pg').Pool;
+const compression = require('compression');
+const express = require('express');
+const multer  = require('multer');
+const app = express();
+const Pool = require('pg').Pool;
 const SQL = require('sql-template-strings');
-var fs = require('fs');
-var config = require(__dirname + '/config.json');
+const fs = require('fs-extra');
+const unzip = require('unzip');
+const ogr2ogr = require('ogr2ogr')
+const g_config = require(__dirname + '/config/config.json');
 
 process.on('unhandledRejection', function(e) {
 	console.log(e.message, e.stack);
 })
 
-var dbpools = {};
-for (db in config.databases) {
+const g_dbpools = {};
+for (db in g_config.databases) {
 	console.log("Setting up pool for database '" + db + "'");
-	dbpools[db] = new Pool(config.databases[db]);
+	g_dbpools[db] = new Pool(g_config.databases[db]);
 }
-var sources = {};
-var sourcedir = __dirname + '/' + config.sourcedir + '/';
-fs.readdirSync(sourcedir).forEach(file => {
+const g_sources = {};
+const g_sourcedir = __dirname + '/' + g_config.sourcedir + '/';
+fs.readdirSync(g_sourcedir).forEach(file => {
 	var matches = file.match(/^(\w+)\.json$/)
 	if (!matches){
 		console.log('Skipping unknown source file: ' + file);
@@ -26,13 +29,13 @@ fs.readdirSync(sourcedir).forEach(file => {
 	console.log('Reading source file ' + file);
 	var sourcename = matches[1];
 	try {
-		var json = fs.readFileSync(sourcedir + file, 'utf8');
+		var json = fs.readFileSync(g_sourcedir + file, 'utf8');
 		var source = JSON.parse(json);
-		if (!dbpools.hasOwnProperty(source.database)) {
+		if (!g_dbpools.hasOwnProperty(source.database)) {
 			console.log("Unknown database '" + source.database + "' in " + file);
 			return;
 		}
-		sources[sourcename] = source;
+		g_sources[sourcename] = source;
 	} catch (err) {
 		console.log('Invalid source file: ' + file);
 	}
@@ -50,26 +53,27 @@ app.get('/mvt/:layers/:z/:x/:y.mvt', function(req, res) {
 	for (var i=0; i<layers.length; i++) {
 		var layer = layers[i];
 		if (!layer.match(/^\w+$/)){
-			console.log('Invalid source name: ' + layer);
+			console.log('Invalid source id');
 			code = 400;
-			reponse = 'Invalid source name: ' + layer;
+			reponse = 'Invalid source id';
 			break;
 		}
-		if (!sources.hasOwnProperty(layer)) {
+		if (!g_sources.hasOwnProperty(layer)) {
 			console.log('Unknown source: ' + layer);
 			code = 404;
 			response = 'Unknown source: ' + layer;
 			break;
 		}
-		var source = sources[layer];
-		var pool = dbpools[source.database];
+		var source = g_sources[layer];
+		var name = source.name;
+		var pool = g_dbpools[source.database];
 		
 		var attributes = source.attributes ? ', ' + source.attributes : '';
 		var join = source.join ? source.join : '';
 		var groupby = source.groupby ? source.groupby : '';
 		
 		var query_text = `
-		SELECT ST_AsMVT('${layer}', 4096, 'geom', q) AS mvt FROM (
+		SELECT ST_AsMVT('${name}', 4096, 'geom', q) AS mvt FROM (
 			SELECT ST_AsMVTGeom(ST_Transform(${source.table}.${source.geometry}, 3857), TileBBox(${z}, ${x}, ${y}, 3857), 4096, 10, true) geom ${attributes}
 			FROM ${source.table}
 			${join}
@@ -116,5 +120,195 @@ app.get('/mvt/:layers/:z/:x/:y.mvt', function(req, res) {
 	}
 });
 
-app.listen(config.port);
-console.log('mvt server is listening on port ' + config.port);
+var allowCORS = function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+//    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+app.use(allowCORS);
+app.use('/html', express.static(__dirname + '/html'));
+
+function unzipUpload(zipfile) {
+	return new Promise(function(resolve, reject) {
+		var output_dir = zipfile + '_unzip';
+		if (!fs.existsSync(output_dir)){
+			fs.mkdirSync(output_dir);
+		}
+		var result = '';
+		fs.createReadStream(zipfile)
+			.pipe(unzip.Parse())
+			.on('entry', function (entry) {
+				var fileName = entry.path;
+				var type = entry.type;
+//				console.log('entry ' + fileName);
+				if (!result && type == 'File' && fileName.match(/^.+\.(json|geojson)$/)) {
+//					console.log('unzipping ' + fileName);
+					entry.pipe(fs.createWriteStream(output_dir + '/' + fileName));
+					result = output_dir + '/' + fileName;
+				} else {
+					entry.autodrain();
+				}
+			})
+			.on('close', function () {
+				console.log('closed zip');
+				if (!result) {
+					reject('No geojson found in zip file'); return;
+				}
+				resolve(result);
+			});
+	});
+}
+
+function loadGeoJson(file) {
+	var promises = [];
+	var database = g_config.uploads.database;
+	var host = g_config.databases[database].host;
+	var port = g_config.databases[database].port;
+	var user = g_config.databases[database].user;
+	var pass = g_config.databases[database].password;
+	var dbname = g_config.databases[database].database;
+	var schema = g_config.uploads.schema;
+	var pgstr = `PG:host=${host} port=${port} user=${user} dbname=${dbname}`;
+	if (pass) pgstr = pgstr + ` password=${pass}`;
+	
+	return new Promise( function(resolve, reject) {
+		console.log('loading geojson');
+		var matches = file.match(/\/(\w+)_unzip\/[^\/]+$/);
+		var table = matches[1];
+		ogr2ogr(file)
+			.format('PostgreSQL')
+			.options(['-nln', schema + '.' + table])
+			.destination(pgstr)
+			.timeout(60000)
+			.exec(function(err) {
+				if (err) {
+					reject(err.message); return;
+				}
+				resolve(table);
+			});
+	});
+}
+
+function makeConfig(table) {
+	return new Promise(function(resolve, reject) {
+		var schema = g_config.uploads.schema;
+		var pool = g_dbpools[g_config.uploads.database];
+		//var query = `SELECT * FROM ${table} WHERE false`;
+		var query = `SELECT column_name, udt_name FROM INFORMATION_SCHEMA.COLUMNS 
+						WHERE table_schema = '${schema}' AND table_name = '${table}'`;
+		pool.query(query).then(result => {
+			var geom_column = '';
+			var fields = result.rows.filter(row => {
+				if (row.udt_name == 'geometry') {
+					geom_column = row.column_name;
+					return false;
+				} else {
+					return true;
+				}
+			}).map(row => {
+				return row.column_name;
+			});
+			if (geom_column) {
+				pool.query(`SELECT Find_SRID('${schema}', '${table}', '${geom_column}') srid, 
+							GeometryType(${geom_column}) geom_type 
+							FROM ${schema}."${table}" LIMIT 1`).then(result => {
+					var srid = result.rows[0].srid;
+					var geom_type = result.rows[0].geom_type;
+					var source_config = {
+						"database": g_config.uploads.database,
+						"table": `${schema}."${table}"`,
+						"geometry": geom_column,
+						"type": geom_type,
+						"srid": srid,
+						"minzoom": 0,
+						"maxzoom": 22,
+						"attributes": fields.join(', ')
+					};
+					var json = JSON.stringify(source_config, null, '\t');
+					
+					fs.writeFile(g_sourcedir + table + '.json', json, err => {
+						if (err) {
+							reject('Error writing source file for id: ' + table);
+						} else {
+							g_sources[table] = source_config;
+							resolve('Successfully uploaded json with id: ' + table);
+						}
+					});
+				});
+			} else {
+				resolve('Unable to find geometry column');
+			}
+		}).catch(reason => {
+			reject('Error executing database query: ' + reason.message);
+		});
+	});
+}
+
+function makeStyle(id){
+	return new Promise(function(resolve, reject) {
+		var source = g_sources[id];
+		var pool = g_dbpools[source.database];
+		var query = `WITH extent AS (
+						SELECT ST_Transform(ST_SetSRID(ST_Extent(wkb_geometry), 28992), 4326) geom FROM ${source.table}
+					)
+					SELECT ST_XMin(geom) xmin, ST_YMin(geom) ymin, ST_XMax(geom) xmax, ST_XMax(geom) ymax FROM extent`;
+		pool.query(query).then(result => {
+			var row = result.rows[0];
+			var xrange = row.xmax - row.xmin;
+			var yrange = row.ymax - row.ymin;
+			var center = [row.xmin + xrange / 2, row.ymin + yrange / 2];
+			var zoom = Math.min(1-Math.log2(xrange/360), 1-Math.log2(yrange/180)) + 1;
+			
+			
+		});
+	});
+}
+
+const upload = multer({ dest: __dirname + '/' + g_config.uploads.directory });
+app.post('/upload', upload.single('file'), function (req, res, next) {
+	//console.log(req.file);
+	function cleanup(path) {
+		fs.remove(path);
+		fs.remove(path + '_unzip');
+	}
+	if (req.file.mimetype == 'application/x-zip-compressed') {
+		unzipUpload(req.file.path)
+			.then(loadGeoJson)
+			.then(makeConfig)
+			.then( result => {
+				console.log(result);
+				res.status(200)
+					.send(result);
+				cleanup(req.file.path);
+			})
+			.catch( reason => {
+				console.log('Error loading geojson: ' + reason);
+				res.status(400)
+					.send('Error loading geojson: ' + reason);
+				cleanup(req.file.path);
+			});
+	} else {
+		res.status(400);
+		res.send('Please upload a zipped geojson.');
+		cleanup(req.file.path);
+	}
+});
+
+
+/*app.get('/upload.html', function(req, res) {
+	fs.readFile(__dirname + '/upload.html', function (err, html) {
+		if (err) {
+			res.status(500);
+			res.send('Error loading upload page');
+		} else {
+			res.status(200);
+			res.header('Content-Type', 'text/html');
+			res.send(html);
+		}
+	});	
+});*/
+
+app.listen(g_config.port);
+console.log('mvt server is listening on port ' + g_config.port);
